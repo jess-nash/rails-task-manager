f63165beeee7805439f5cccde785c58b1465f5e4
Jessica Nash <jeenash2@gmail.com>
2022-07-02 13:52:14 +0900

    pre-fefactor

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,54 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+
+    # no need for app/views/restaurants/create.html.erb
+    redirect_to task_path(@task)
+  end
+
+  def show
+    # @task = Task.find(params[:id])
+    @check = @task.completed ? '[X]' : '[ ]'
+  end
+
+  def edit
+    # @task = Task.find(params[:id])
+  end
+
+  def update
+    # @task = Task.find(params[:id])
+    @task.update(task_params)
+
+    # no need for app/views/restaurants/create.html.erb
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    # @task = Task.find(params[:id])
+    @task.destroy
+
+    # no need for app/views/restaurants/destroy.html.erb
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,21 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # As a user, I can list tasks
-  # As a user, I can view the details of a task
-  # As a user, I can add a new task
-  # As a user, I can edit a task (mark as completed / update title & details)
-  # As a user, I can remove a task
+  # # As a user, I can list tasks
+  # get 'tasks', to: 'tasks#index', as: :tasks
+
+  # # As a user, I can add a new task (GOES BEFORE VIEW)
+  # get 'tasks/new', to: 'tasks#new', as: :new_task
+  # post 'tasks', to: 'tasks#create'
+
+  # # NB: The `show` route needs to be *after* `new` route.
+  # # As a user, I can view the details of a task
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # # As a user, I can edit a task (mark as completed / update title & details)
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # patch 'tasks/:id', to: 'tasks#update'
+
+  # # As a user, I can remove a task
+  # delete 'tasks/:id', to: 'tasks#destroy'
+
+  resources :tasks
 end
