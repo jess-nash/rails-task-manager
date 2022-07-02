366c1a8de5de3b9377a514b30a88b8bbf335c4f7
Jessica Nash <jeenash2@gmail.com>
2022-07-02 11:36:31 +0900

    Add task model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # As a user, I can list tasks
+  # As a user, I can view the details of a task
+  # As a user, I can add a new task
+  # As a user, I can edit a task (mark as completed / update title & details)
+  # As a user, I can remove a task
 end
